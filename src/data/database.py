3f93082d73570b75d6fe90ff1e3fc4473831c90d
tayadj <tayadj@users.noreply.github.com>
2025-03-19 12:57:51 +0300

    Implemented local database storage

--- src/data/database.py
+++ src/data/database.py
@@ -1,48 +1,72 @@
 import aiohttp
+import json
 import googleapiclient
 import google.oauth2.service_account
 import google.auth.transport.requests
+import os
+import pandas
 
 
 
 class Database:
 
-	def __init__(self, google_service_account: dict, ):
+	class GoogleDatabase:
+
+		def __init__(self, google_service_account: dict):
+
+			self.google_service_account = google_service_account
+			self.access_token = None
+			self.request = None
+			self.credentials = google.oauth2.service_account.Credentials.from_service_account_info(
+				self.google_service_account,
+				scopes = ['https://www.googleapis.com/auth/spreadsheets']
+			)
+
+		async def authentificate(self):
+
+			self.request = google.auth.transport.requests.Request()
+			self.credentials.refresh(self.request)
+			self.access_token = self.credentials.token
+
+		async def load(self, url: str):
+
+			if not self.access_token:
+
+				await self.authentificate()
+
+			async with aiohttp.ClientSession() as session:
+
+				async with session.get(url, headers = {'Authorization' : f'Bearer {self.access_token}'}) as response:
+
+					if response.status == 200:
+
+						data = await response.json()
+						data = data.get('values', [])
+
+					else:
+
+						print(f"Failed to load data: {response.status} - {await response.text()}")
+						data = []
+		
+			return data
+
+	class LocalDatabase:
+
+		def __init__(self, path: str):
+
+			self.path = path
+
+		async def load(self, id: str):
+
+			with open(f'{os.path.dirname(__file__)}/{self.path}/{id}.json', 'r', encoding = 'utf-8') as file:
+			
+				data = json.load(file)
+
+			return data
+
+	def __init__(self, google_service_account: dict, local_database_path: str):
 
 		self.google_service_account = google_service_account
-		self.access_token = None
-		self.request = None
-		self.credentials = google.oauth2.service_account.Credentials.from_service_account_info(
-			self.google_service_account,
-			scopes = ['https://www.googleapis.com/auth/spreadsheets']
-		)
-
-	async def authentificate(self):
-
-		self.request = google.auth.transport.requests.Request()
-		self.credentials.refresh(self.request)
-		self.access_token = self.credentials.token
-
-		print(self.request, self.credentials, self.access_token)
-
-	async def load(self, url: str):
-
-		if not self.access_token:
-
-			await self.authentificate()
-
-		async with aiohttp.ClientSession() as session:
-
-			async with session.get(url, headers = {'Authorization' : f'Bearer {self.access_token}'}) as response:
-
-				if response.status == 200:
-
-					data = await response.json()
-					data = data.get('values', [])
-
-				else:
-
-					print(f"Failed to load data: {response.status} - {await response.text()}")
-					data = []
-		
-		return data
+		self.local_database_path = local_database_path
+		self.google_database = self.GoogleDatabase(self.google_service_account)
+		self.local_database = self.LocalDatabase(self.local_database_path)
--- src/main.py
+++ src/main.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
 
 		settings = config.Settings()
 		engine = core.service.Engine(settings.OPENAI_API_TOKEN.get_secret_value())
-		database = data.Database(settings._GOOGLE_SERVICE_ACCOUNT())
+		database = data.Database(settings._GOOGLE_SERVICE_ACCOUNT(), '/storage')
 		
-		print(await database.load(settings.GOOGLE_SERVICE_DATABASE_URL.get_secret_value()))
+		#print(await database.google_database.load(settings.GOOGLE_SERVICE_DATABASE_URL.get_secret_value()))
+		#print(await database.local_database.load('database'))
 
 	asyncio.run(main())
