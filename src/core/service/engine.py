2c29a1ba9cca5e3c28b8241523b1535b7a81adbb
tayadj <tayadj@users.noreply.github.com>
2025-03-19 13:17:44 +0300

    Implemented engine base structure

--- src/core/service/engine.py
+++ src/core/service/engine.py
@@ -1,7 +1,22 @@
-
+import asyncio
+import llama_index
+import llama_index.llms.openai
 
 class Engine():
 
+	class RAGPipeline():
+
+		# RAG Strategy 
+		pass
+
 	def __init__(self, openai_api_token: str):
+
+		self.openai_api_token = openai_api_token
+		self.model = llama_index.llms.openai.OpenAI (
+			model = 'gpt-3.5-turbo',
+			api_key = openai_api_token
+		)
+
+	def request(self, query: str):
 
 		pass
