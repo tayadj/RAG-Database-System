ae7407c4f7751a2e0ca482326dc965c5380e2f61
tayadj <tayadj@users.noreply.github.com>
2025-03-19 13:47:11 +0300

    Implemented vector document storage

--- src/core/service/engine.py
+++ src/core/service/engine.py
@@ -4,18 +4,49 @@ import llama_index.llms.openai
 
 class Engine():
 
+	# implement multi-thread
+
 	class RAGPipeline():
+
+		
 
 		# RAG Strategy 
 		pass
 
-	def __init__(self, openai_api_token: str):
+	def __init__(self, openai_api_token: str, RAG_data: dict):
 
+		self.data = RAG_data
+		self.index = None
+
+		'''
 		self.openai_api_token = openai_api_token
 		self.model = llama_index.llms.openai.OpenAI (
 			model = 'gpt-3.5-turbo',
 			api_key = openai_api_token
 		)
+		'''
+
+		self.setup_storage()
+
+	# move datalake strategy to /data ?
+
+	# rearrange data ingestion strategy to be universal
+
+	def setup_storage(self):
+
+		documents = [
+			llama_index.core.Document(
+				text = chunk.get('text',''),
+				metadata = chunk.get('metadata',{})
+			)
+			for file in self.data
+			for chunk in file['chunks']
+		] 
+
+		print(documents[:5])
+
+		self.index = llama_index.core.GPTVectorStoreIndex.from_documents(documents)
+		self.index.storage_context.persist(persist_dir = '/storage_context')
 
 	def request(self, query: str):
 
--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
 	async def main():
 
 		settings = config.Settings()
-		engine = core.service.Engine(settings.OPENAI_API_TOKEN.get_secret_value())
+
 		database = data.Database(settings._GOOGLE_SERVICE_ACCOUNT(), '/storage')
-		
-		#print(await database.google_database.load(settings.GOOGLE_SERVICE_DATABASE_URL.get_secret_value()))
-		#print(await database.local_database.load('database'))
+		RAG_data = await database.local_database.load('database')
+
+		engine = core.service.Engine(settings.OPENAI_API_TOKEN.get_secret_value(), RAG_data)
 
 	asyncio.run(main())
