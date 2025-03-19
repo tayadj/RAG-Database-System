526b78c363ffc913e622da6c90709dfaaeff8a49
tayadj <tayadj@users.noreply.github.com>
2025-03-19 22:26:47 +0300

    Implemented test ragas evaluation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import core
 import data
 
 import asyncio
+import ragas
 
 
 
@@ -14,26 +15,38 @@ if __name__ == '__main__':
 		database = data.Database(settings._GOOGLE_SERVICE_ACCOUNT(), settings.LOCAL_DATABASE_URL.get_secret_value())
 		engine = core.service.Engine(settings.OPENAI_API_TOKEN.get_secret_value(), (await database.local_database.load('database')))
 
-		dataframe = (await database.google_database.load(settings.GOOGLE_DATABASE_URL.get_secret_value()))[:3]
+		evaluator_llm = ragas.llms.LlamaIndexLLMWrapper(engine.model)
+
+		dataframe = (await database.google_database.load(settings.GOOGLE_DATABASE_URL.get_secret_value()))[:2]
 
 		for record in dataframe:
 
 			record.append(await engine.request(record[0]))
 
-
-
-
 		print(dataframe)
-		
+
+		# move to evaluator
+
+		sample = ragas.dataset_schema.SingleTurnSample(
+			user_input = dataframe[0][0],
+			response = dataframe[0][1],
+			reference = '', # ground_truth
+			retrieved_contexts = [] # chunks contexts
+		)
+
+		factual_correctness_scorer = ragas.metrics._factual_correctness.FactualCorrectness(
+			llm = evaluator_llm
+		)
+		noise_sensitivity_scorer = ragas.metrics.NoiseSensitivity(
+			llm = evaluator_llm
+		)
+
+		print(await factual_correctness_scorer.single_turn_ascore(sample))
+		print(await noise_sensitivity_scorer.single_turn_ascore(sample))
+
 		await database.google_database.save(
 			settings.GOOGLE_DATABASE_URL.get_secret_value(),
 			dataframe
 		)
-
-		#RAG_data = await database.local_database.load('database')
-
-		#engine = core.service.Engine(settings.OPENAI_API_TOKEN.get_secret_value(), RAG_data)
-
-		#await engine.request('How can I better manage my stock to avoid running out of popular items?')
 
 	asyncio.run(main())
