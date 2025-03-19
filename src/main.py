2578d19610a0fe86540cf6d7f7dbe69c318202be
tayadj <tayadj@users.noreply.github.com>
2025-03-19 11:33:03 +0300

    Added engine declaration

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import config
+import core
 
 import asyncio
+
+
+
+
 
 if __name__ == '__main__':
 
 	async def main():
 
 		settings = config.Settings()
-		print(settings.GOOGLE_SERVICE.get_secret_value())
+		engine = core.service.Engine(settings)
 
 	asyncio.run(main())
